797ff132cd626dc5eeb2c518580ff1e0f6ea1bb9
libin <libinbin9456@126.com>
2023-11-10 17:09:05 +0800

    remove test code

--- Source/OxyplotMauiSample/App.xaml.cs
+++ Source/OxyplotMauiSample/App.xaml.cs
@@ -7,22 +7,5 @@
             InitializeComponent();
             MainPage = new AppShell();
         }
-
-        //private async void InitFonts()
-        //{
-        //    var cacheDir = FileSystem.Current.CacheDirectory;
-        //    var fontsDir = Path.Combine(cacheDir, "oxyplot_fonts");
-        //    //MauiPlotSetting.CustomFontsDirectory = fontsDir;
-
-        //    var fontFileName = "NotoSansCJKsc-Regular.otf";
-        //    var fontFileFullPath = Path.Combine(fontsDir, fontFileName);
-        //    if (File.Exists(fontFileFullPath))
-        //        return;
-
-        //    Directory.CreateDirectory(fontsDir);
-        //    var fs = await FileSystem.OpenAppPackageFileAsync(fontFileName);
-        //    await using var fileStream = File.Create(fontFileFullPath);
-        //    await fs.CopyToAsync(fileStream);
-        //}
     }
 }
