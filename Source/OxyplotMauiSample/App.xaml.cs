18335f2858c0bf49ace8ee46b36f9a3178b5685e
Janus Weil <janus@gcc.gnu.org>
2025-04-01 17:23:17 +0200

    OxyplotMauiSample: fix a warning
    'Application.MainPage.set' is obsolete: 'This property is deprecated. Initialize your application by overriding Application.CreateWindow rather than setting MainPage. To modify the root page in an active application, use Windows[0].Page for applications with a single window. For applications with multiple windows, use Application.Windows to identify and update the root page on the correct window.  Additionally, each element features a Window property, accessible when it's part of the current window.'

--- Source/OxyplotMauiSample/App.xaml.cs
+++ Source/OxyplotMauiSample/App.xaml.cs
@@ -1,11 +1,16 @@
-﻿namespace OxyplotMauiSample
+﻿
+namespace OxyplotMauiSample
 {
     public partial class App
     {
         public App()
         {
             InitializeComponent();
-            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            return new Window(new AppShell());
         }
     }
 }
